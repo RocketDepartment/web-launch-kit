e1213ce4aa52be4f71bb5fb8cfa55aa56df910ba
andy <andy@rocketdept.com>
2014-09-08 16:38:18 -0700

    updated nodemon settings

--- gulp/tasks/nodemon.js
+++ gulp/tasks/nodemon.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
 gulp.task('nodemon', function () {
-   nodemon({ script: 'server/app.js', ext: 'html js', ignore: ['app/**','build/**','gulp/**'] });
+   nodemon({ script: 'server/app.js', ext: 'html js', ignore: ['app/**','build/**','gulp/**','node_modules/**','bower_components/**'] });
 });
